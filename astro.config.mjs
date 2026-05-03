a717330fd28585c3b8aead62464fa0a66380e093
Rubén Tipiani <38152657+rtipiani@users.noreply.github.com>
2026-05-02 19:01:37 -0500

    feat: add sitemap integration

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,17 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 import tailwindcss from '@tailwindcss/vite';
+import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
+    site: 'https://hotelchaskavallegreen.com/',
     build: {
         inlineStylesheets: 'always'
     },
     vite: {
         // @ts-ignore
         plugins: [tailwindcss()]
-    }
+    },
+    integrations: [sitemap()]
 });
